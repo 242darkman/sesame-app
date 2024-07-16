655752a491bfa0ccd38c2728d40664ed3eb60f04
Laura-Fa <laurafauvet@hotmail.fr>
2024-07-16 13:54:03 +0200

    menu

---
+++ client_app/lib/widgets/custom_menu.dart
@@ -0,0 +1,58 @@
+import 'package:client_app/screens/scanner/scanner_screen.dart';
+import 'package:flutter/material.dart';
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border(top: BorderSide(color: Color(0xFFA8A8A8))),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+            child: IconButton(
+              icon: const Icon(
+                Icons.qr_code_scanner,
+                color: Color(0xFF779DA0),
+                size: 35.0,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ScannerScreen()),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+            child: IconButton(
+              icon: const Icon(
+                Icons.question_mark,
+                color: Color(0xFF779DA0),
+                size: 35.0,
+              ),
+              onPressed: () {},
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+            child: IconButton(
+              icon: const Icon(
+                Icons.settings,
+                color: Color(0xFF779DA0),
+                size: 35.0,
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
