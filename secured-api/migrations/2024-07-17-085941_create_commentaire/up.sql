af5b242701afff5e4b6c659e03e24f09d5745f77
dmariama899 <dmariama899@gmail>
2024-07-17 11:09:35 +0200

    Ajout de la table comment

---
+++ secured-api/migrations/2024-07-17-085941_create_commentaire/up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE comment(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+    dateComment TIMESTAMP NOT NULL,
+    interventionStatus VARCHAR NOT NULL,
+    comments VARCHAR NOT NULL,
+    idUser UUID REFERENCES users(id),
+    idIntervention UUID REFERENCES intervention(id),
+);
